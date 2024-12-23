d96235b3abaef1d88f67ea3b860f4044e4f0c21c
aymane <choukriayman7@gmail.com>
2024-12-23 21:11:16 +0100

    change programme

--- math.c
+++ math.c
@@ -1,36 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-void calcule(int n1, int n2, int *rusélta, char coriction) {
+void calcule(int n1, int n2, double *rusélta, char coriction) {
     switch (coriction) {
         case '+':
             *rusélta = n1 + n2;
-            printf("résultat : %d\n", *rusélta);
-            break;
+             printf("résultat : %.2f\n", *rusélta);
+             break;
         case '-':
-            *rusélta = n1 - n2;
-            printf("résultat : %d\n", *rusélta);
-            break;
+             *rusélta = n1 - n2;
+             printf("résultat : %.2f\n", *rusélta);
+             break;
         case '*':
             *rusélta = n1 * n2;
-            printf("résultat : %d\n", *rusélta);
-            break;
+             printf("résultat : %.2f\n", *rusélta);
+             break;
         case '/':
             if (n2 != 0) {
-                *rusélta = n1 / n2;
-                printf("résultat : %d\n", *rusélta);
+                *rusélta = (double)n1 / n2;
+                 printf("résultat : %.2f\n", *rusélta);
             } else {
-                printf("erreur : division par zéro\n");
+                 printf("erreur : division par zéro\n");
             }
             break;
         case '%':
             if (n2 != 0) {
                 *rusélta = n1 % n2;
-                printf("résultat : %d\n", *rusélta);
+                 printf("résultat : %.2f\n", *rusélta);
             } else {
-                printf("erreur : division par zéro\n");
+                 printf("erreur : division par zéro\n");
             }
             break;
+        case '^':
+              *rusélta = pow(n1 ,n2);
+               printf("résulta :%.2f\n" ,*rusélta);  
+               break;
         default:
             printf("l'opération n'est pas valide\n");
             break;
@@ -38,16 +43,17 @@ void calcule(int n1, int n2, int *rusélta, char coriction) {
 }
 
 int main() {
-    int n1, n2, rusélta;
+    int n1, n2;
+    double rusélta;
     char coriction;
 
     printf("Entrez le nombre 1 :\n");
     scanf("%d", &n1);
-
+  
     printf("Entrez le nombre 2 :\n");
     scanf("%d", &n2);
 
-    printf("Entrez l'opération ('+', '-', '*', '/', '%%') :\n");
+    printf("Entrez l'opération ('+', '-', '*', '/', '%%' ,'^') :\n");
     scanf(" %c", &coriction);
 
     calcule(n1, n2, &rusélta, coriction);  
